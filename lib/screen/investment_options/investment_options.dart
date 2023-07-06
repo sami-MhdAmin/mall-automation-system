74a920f2451c6a41b100ac0fce62f0974b401db0
Sami <sami.amin.6.2001@gmail.com>
2023-07-06 15:18:08 +0300

    sam

--- lib/screen/investment_options/investment_options.dart
+++ lib/screen/investment_options/investment_options.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:jessy_mall/helpers/app_colors.dart';
 import 'package:jessy_mall/screen/investment_options/widgets/investment_store.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class InvestmentOptions extends StatelessWidget {
-  const InvestmentOptions({super.key});
+  InvestmentOptions({super.key});
   TabBar get _tabBar => const TabBar(
         tabs: [
           Tab(icon: Icon(Icons.store_mall_directory)),
@@ -55,6 +56,26 @@ class Listing {
 }
 
 class InvestmentStandUI extends StatelessWidget {
+  // Create a webview controller
+  final _controller = WebViewController()
+    ..setJavaScriptMode(JavaScriptMode.unrestricted)
+    ..setNavigationDelegate(
+      NavigationDelegate(
+        onProgress: (int progress) {
+          // print the loading progress to the console
+          // you can use this value to show a progress bar if you want
+          debugPrint("Loading: $progress%");
+        },
+        onPageStarted: (String url) {},
+        onPageFinished: (String url) {},
+        onWebResourceError: (WebResourceError error) {},
+        onNavigationRequest: (NavigationRequest request) {
+          return NavigationDecision.navigate;
+        },
+      ),
+    )
+    ..loadRequest(
+        Uri.parse("https://app.vectary.com/p/67QoljN0bz9vp78OSy1tti"));
   final List<Listing> listings = [
     Listing(
       space: '120 sq. ft.',
@@ -74,26 +95,31 @@ class InvestmentStandUI extends StatelessWidget {
     return Scaffold(
         body: Padding(
       padding: EdgeInsets.all(16.0),
-      child: Card(
-        elevation: 4.0,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(16.0),
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Padding(
-              padding: EdgeInsets.all(16.0),
-              child: Text(
-                  "In this option you can immediately invest until 5 stands,\n every stand have one metter space, and you can have a type in each stand."),
+      child: Column(
+        children: [
+          SizedBox(height: 200, child: WebViewWidget(controller: _controller)),
+          Card(
+            elevation: 4.0,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(16.0),
             ),
-            Padding(
-              padding: EdgeInsets.all(16.0),
-              child: Text(
-                  "Available types\n price per month \n types: kitchen stuff, Detergent, canned food."),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: Text(
+                      "In this option you can immediately invest until 5 stands,\n every stand have one metter space, and you can have a type in each stand."),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: Text(
+                      "Available types\n price per month \n types: kitchen stuff, Detergent, canned food."),
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     ));
   }
