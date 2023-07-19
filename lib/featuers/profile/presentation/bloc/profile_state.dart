3a8a3fe81c7c41536840359b96bfaab82555c933
Salim-Jabbour <salimwjabbour@gmail.com>
2023-07-19 16:03:02 +0300

    done with profile_page and store_page

--- lib/featuers/profile/presentation/bloc/profile_state.dart
+++ lib/featuers/profile/presentation/bloc/profile_state.dart
@@ -1,8 +1,7 @@
-
 //TODO: somehting todo with logout
 // class ProfileLogout
 
-//TODO might be 2 states for every button and event 
+//TODO might be 2 states for every button and event
 
 part of 'profile_bloc.dart';
 
@@ -22,4 +21,24 @@ class ProfileLoaded extends ProfileState {
   final ProfileModel profileModel;
 
   ProfileLoaded({required this.profileModel});
+}
+
+class ProfileOrderLoaded extends ProfileState {
+  //TODO: order model
+}
+
+class ProfileShippingAdressesLoaded extends ProfileState {
+  //TODO: shipping addresses model
+}
+
+class ProfileInvestStoreOrStandLoaded extends ProfileState {
+  //TODO: Investstore or stand model
+}
+
+class ProfileSettingsLoaded extends ProfileState {
+  //TODO: Settings model
+}
+
+class ProfileManageMyStoresLoaded extends ProfileState {
+  //TODO: manage my store model
 }
