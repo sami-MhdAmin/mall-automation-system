7dfcffc4d4c3dec362b488612a9c4457e879c7fb
Salim-Jabbour <salimwjabbour@gmail.com>
2023-08-18 20:46:12 +0300

    slider home

--- lib/featuers/Favorite/presentation/pages/favorite.dart
+++ lib/featuers/Favorite/presentation/pages/favorite.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get_it/get_it.dart';
 import 'package:jessy_mall/featuers/Favorite/presentation/widgets/favorite_body.dart';
-import 'package:jessy_mall/featuers/cart/presentation/widgets/my_cart_body.dart';
 
-import '../BLoC/bloc/favorite_bloc.dart';
+
+import '../BLoC/favorite/favorite_bloc.dart';
 
 class FavoritePage extends StatelessWidget {
   const FavoritePage({super.key});
@@ -13,7 +13,7 @@ class FavoritePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocProvider(
       create: (context) => GetIt.I.get<FavoriteBloc>(),
-      child:  Scaffold(
+      child: Scaffold(
         body: FavoriteBody(),
       ),
     );
