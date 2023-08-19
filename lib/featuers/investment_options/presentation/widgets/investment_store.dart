b3340ea9ebf5db8209b7fc1df7f7fb2c1916c9f3
Sami <sami.amin.6.2001@gmail.com>
2023-08-20 02:38:09 +0300

    refactor link payment to take store with our server

--- lib/featuers/investment_options/presentation/widgets/investment_store.dart
+++ lib/featuers/investment_options/presentation/widgets/investment_store.dart
@@ -8,25 +8,10 @@ import 'package:jessy_mall/featuers/investment_options/presentation/bloc/invest_
 import 'package:jessy_mall/featuers/investment_options/presentation/page/invest_store_ui.dart';
 import 'package:jessy_mall/featuers/investment_options/presentation/page/invest_store_page.dart';
 import 'package:easy_localization/easy_localization.dart';
+import 'package:jessy_mall/featuers/investment_options/presentation/widgets/card_store.dart';
 import '../../../../core/utils/global_snackbar.dart';
 import '../../../../core/widgets/loading_widget.dart';
 import '../../../Auth/presintation/bloc/auth_bloc.dart';
-
-class Listing {
-  final String photoUrl;
-  final String floor;
-  final String space;
-  final String type;
-  final String price;
-
-  Listing({
-    required this.photoUrl,
-    required this.floor,
-    required this.space,
-    required this.type,
-    required this.price,
-  });
-}
 
 class InvestmentStoreUI extends StatelessWidget {
   /* final List<Listing> listings = [
@@ -61,7 +46,7 @@ class InvestmentStoreUI extends StatelessWidget {
           // TODO: implement listener
           if (state is InvestStoreOptionFailed) {
             gShowErrorSnackBar(
-                context: context, message: "something went wrong");
+                context: context, message: StringManager.sthWrong.tr());
           }
           if (state is InvestStoreOptionSuccessd) {
             // print(state.profileModel.profileDataModel?.email);
@@ -102,71 +87,6 @@ class InvestmentStoreUI extends StatelessWidget {
             ],
           );
         },
-      ),
-    );
-  }
-}
-
-class CardStore extends StatelessWidget {
-  const CardStore({super.key, required this.listings, required this.index});
-
-  final List<InvestStoreDataModel> listings;
-  final int index;
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      elevation: 4.0,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(16.r),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          ClipRRect(
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(30.0.r),
-              topRight: Radius.circular(30.0.r),
-            ),
-            child: Image.network(
-              'https://image.cnbcfm.com/api/v1/image/103448059-GettyImages-492843021.jpg?v=1675861201&w=740&h=416&ffmt=webp&vtcrop=y',
-              height: 600.0.h,
-              fit: BoxFit.cover,
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  listings[index].type ?? "",
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 55.0.sp,
-                  ),
-                ),
-                const SizedBox(height: 8.0),
-                Text(
-                  '${StringManager.floor.tr()} ${listings[index].floor}',
-                  style: TextStyle(fontSize: 48.0.r),
-                ),
-                const SizedBox(height: 8.0),
-                Text(
-                  '${StringManager.storeSpace.tr()} ${listings[index].store_space}',
-                  style: TextStyle(fontSize: 48.0.sp),
-                ),
-                const SizedBox(height: 16.0),
-                Text(
-                  "${listings[index].price}\$",
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 55.0.sp,
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
       ),
     );
   }
