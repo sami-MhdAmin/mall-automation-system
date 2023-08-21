9d90d978408bc1067c8f21c0efef3c3baaed7148
Salim-Jabbour <salimwjabbour@gmail.com>
2023-08-21 22:10:23 +0300

    refactor

---
+++ lib/featuers/cart/models/cart_model.dart
@@ -0,0 +1,14 @@
+class CartModel{
+
+final CartOrderDataModel? data;
+final String? message;
+
+  CartModel(this.data, this.message);
+}
+
+class CartOrderDataModel{
+
+}
+
+class CartSingleProductModel{
+}
